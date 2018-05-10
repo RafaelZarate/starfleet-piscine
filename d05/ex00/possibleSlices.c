c726e4657500ad44c7f5b7756451984d4c9e96a0
Rafael Zarate <rafazrte@gmail.com>
2018-05-10 14:26:00 -0700

    Adding files

--- d05/ex00/possibleSlices.c
+++ d05/ex00/possibleSlices.c
@@ -3,7 +3,13 @@
 
 void	findPossibleSlices(t_arr *list, int index, int pizzaSize, int reduced)
 {
-	
+	if (reduced < 0)
+		return ;
+	// if (reduced == 0)
+	// {
+
+	// }
+	int	prev = (index == 0) ? 1: list[]
 }
 
 void	printPossibleSlices(int pizzaSize)
